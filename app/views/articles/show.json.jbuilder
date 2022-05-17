d4db20fa16328ea645f2b7213326b4f575d60eea
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-17 14:51:56 +0200

    everything working fine

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,3 @@
+# json.partial! "articles/article", article: @article
+json.extract! @article, :id, :title, :content, :created_at, :updated_at
+json.url article_url(@article, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Books::Lovecraft.sentence,
+    content: Faker::Books::Lovecraft.paragraph_by_chars
+  )
+end
